14d65f58c91b5c682b4880ad047233f202064d71
Chiesaac7 <isoportosa@est.utn.ac.cr>
2026-06-26 14:21:34 -0600

    Se realiza el paquete Personas
    en el paquete personas se realizan las clases de participantes y responsables

---
+++ src/personas/Persona.java
@@ -0,0 +1,48 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package personas;
+import java.time.LocalDate;
+/**
+ *
+ * @author isaac
+ */
+public class Persona {
+    
+    protected String ID;
+    protected String Nombre;
+    protected LocalDate fechaNacimiento;
+    protected String Telefono;
+
+    public String getID() {
+        return ID;
+    }
+
+    public String getNombre() {
+        return Nombre;
+    }
+
+    public LocalDate getFechaNacimiento() {
+        return fechaNacimiento;
+    }
+
+    public String getTelefono() {
+        return Telefono;
+    }
+
+    public void setTelefono(String Telefono) {
+        this.Telefono = Telefono;
+    }
+
+    public Persona(String ID, String Nombre, LocalDate fechaNacimiento, String Telefono) {
+        this.ID = ID;
+        this.Nombre = Nombre;
+        this.fechaNacimiento = fechaNacimiento;
+        this.Telefono = Telefono;
+    }
+    
+    
+    
+    
+}
---
+++ src/personas/Responsable.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package personas;
+
+/**
+ *
+ * @author isaac
+ */
+public class Responsable {
+    
+    
+    private String especialidad;
+    private String AñosExperiencia;
+}
