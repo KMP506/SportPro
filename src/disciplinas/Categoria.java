f737ef620661d266132083a7d00bb71a48b6e5e7
Kevin <kevinmejias866@gmail.com>
2026-06-26 15:10:55 -0600

    Se desarrolla Metodos de acceso
    Se desarrollaron metodos de acceso en la clase categoria con contructor y toString

--- src/disciplinas/Categoria.java
+++ src/disciplinas/Categoria.java
@@ -9,5 +9,34 @@ package disciplinas;
  * @author kevin
  */
 public class Categoria {
+    protected String nombre;
+    protected String categoria;
+    protected int capacidadMaxima;
+    
+    public String getNombre(){
+        return this.nombre;
+    }
+    
+    public String getCategoria(){
+        return this.categoria;
+    }
+    
+    public int getCapacidadMaxima(){
+        return this.capacidadMaxima;
+    }
+    
+    public void setCapacidadMaxima(int capacidadMaxima){
+        this.capacidadMaxima=capacidadMaxima;
+    }
+    
+    public Categoria(String nombre,String categoria, int capacidadMaxima){
+        this.nombre=nombre;
+        this.categoria=categoria;
+        this.capacidadMaxima=capacidadMaxima;
+    }
+    
+    public String toString(){
+       return "Nombre:"+nombre+"\nCategoria:"+categoria+"\nCapacidad Maxima:"+capacidadMaxima;
+   }
     
 }
